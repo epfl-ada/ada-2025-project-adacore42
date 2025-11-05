c0601b66af54d6e1c822c475b10bb516686ffa44
TheImprovedMuffinMan <andras.horkay@gmail.com>
2025-11-05 15:55:30 +0100

    pruhh

--- src/data/extract_nouns.py
+++ src/data/extract_nouns.py
@@ -1,7 +1,15 @@
 import spacy
-
+import pickle
+import os
 nlp = spacy.load('en_core_web_sm')
 
+def load_data(filepath ='../../data/cleaned_data_prepared.pkl'):
+    if os.path.exists(filepath) == False:
+        raise FileNotFoundError(f"File not found: {filepath}")
+    
+    with open(filepath, "rb") as f:
+        data = pickle.load(f)
+    return data["dataA"], data["dataC"], data["dataA_startID"], data["dataA_endID"], data["dataC_lastGoodID"]
 
 def extract_nouns(text):
     # Ensure the input is a string, not a list
@@ -9,3 +17,30 @@ def extract_nouns(text):
         text = " ".join(text)
     doc = nlp(text.lower())
     return [token.text for token in doc if token.pos_ in ("NOUN", "PROPN")]
+
+def apply_noun_extraction(dataA):
+    #create copy of dataA to store noun-extracted captions
+    dataA_cleaned0 = dataA.copy()
+    for i, df in enumerate(dataA_cleaned0):
+        df['captions_nouns'] = df['cleaned_caption'].apply(extract_nouns)
+        dataA_cleaned0[i] = df
+        print(f"Extracting nouns .......... {(i+1)/len(dataA_cleaned0):.2%} complete.")
+    return dataA_cleaned0
+
+# saving into pickle file
+def save_noun_data(dataA_cleaned0, dataC_cleaned0, dataA_startID, dataA_endID, dataC_lastGoodID, filepath='../../data/cleaned_data_nouns_copy.pkl'):
+    with open(filepath, "wb") as f:
+        pickle.dump({
+            "dataA_nouns": dataA_cleaned0,
+            "dataC_nouns": dataC_cleaned0,
+            "dataA_startID": dataA_startID,
+            "dataA_endID": dataA_endID,
+            "dataC_lastGoodID": dataC_lastGoodID
+        }, f)
+    print("Noun-extracted data saved successfully.")
+
+if __name__ == "__main__":
+    dataA, dataC, dataA_startID, dataA_endID, dataC_lastGoodID = load_data(filepath='../../data/cleaned_data_prepared.pkl')
+    dataA_cleaned0 = apply_noun_extraction(dataA)
+    dataC_cleaned0 = dataC  # Assuming no changes to dataC
+    save_noun_data(dataA_cleaned0, dataC_cleaned0, dataA_startID, dataA_endID, dataC_lastGoodID, filepath='../../data/cleaned_data_nouns_copy.pkl')
--- src/data/tokenise.py
+++ src/data/tokenise.py
@@ -8,6 +8,8 @@ import spacy
 import textblob as TextBlob
 import contractions
 import string
+import os
+import pickle
 
 
 nltk.download('punkt')       # Tokeniser
@@ -17,6 +19,15 @@ nlp = spacy.load('en_core_web_sm')
 
 stop_words = set(stopwords.words('english')) # Initialise stopwords
 lemmatizer = WordNetLemmatizer() # Initialise lemmatiser
+
+def load_data(filepath = '../../data/data_prepared.pkl'):
+    if os.path.exists(filepath) == False:
+        raise FileNotFoundError(f"File not found: {filepath}")
+    
+    with open(filepath, "rb") as f:
+        data = pickle.load(f)
+    return data["dataA"], data["dataC"], data["dataA_startID"], data["dataA_endID"], data["dataC_lastGoodID"]
+
 
 def preprocess_text_list(entry, min_len=2):
     """
@@ -59,3 +70,42 @@ def preprocess_text_list(entry, min_len=2):
     tokens = [lemmatizer.lemmatize(word) for word in tokens]
 
     return ' '.join(tokens)
+
+
+# apply preprocessing to captions in dataA
+def apply_preprocessing(dataA, dataC):
+    size = len(dataA)
+    
+    #create copy of dataA to store cleaned captions
+    dataA1, dataC1 = dataA.copy(), dataC.copy()
+
+    for i, df in enumerate(dataA1):
+        df['cleaned_caption'] = df['caption'].apply(preprocess_text_list)
+        print(f"done with {(i+1)/size*100:.2f}%")
+    
+    # apply preprocessing to image_locations, questions, image_uncanny_descriptions, image_descriptions
+    dataC1['cleaned_image_locations'] = dataC1['image_locations'].apply(preprocess_text_list)
+    print("done with image_locations")
+    dataC1['cleaned_questions'] = dataC1['questions'].apply(preprocess_text_list)
+    print("done with questions")
+    dataC1['cleaned_image_uncanny_descriptions'] = dataC1['image_uncanny_descriptions'].apply(preprocess_text_list)
+    print("done with image_uncanny_descriptions")
+    dataC1['cleaned_image_descriptions'] = dataC1['image_descriptions'].apply(preprocess_text_list)
+    print("done with image_descriptions")
+    return dataA1, dataC1
+
+def save_data(dataA, dataC, dataA_startID, dataA_endID, dataC_lastGoodID, filepath='../../data/cleaned_data_prepared_copy.pkl'):
+    with open(filepath, "wb") as f:
+        pickle.dump({
+            "dataA_startID": dataA_startID,
+            "dataA_endID": dataA_endID,
+            "dataC_lastGoodID": dataC_lastGoodID,
+            "dataA": dataA,
+            "dataC": dataC
+        }, f)
+    print(f"Cleaned data saved to {filepath}")
+
+if __name__ == "__main__":
+    dataA, dataC, dataA_startID, dataA_endID, dataC_lastGoodID = load_data(filepath='../../data/data_prepared.pkl') # Load data
+    dataA1, dataC1 = apply_preprocessing(dataA, dataC) # Tokenise and clean data
+    save_data(dataA1, dataC1, dataA_startID, dataA_endID, dataC_lastGoodID, filepath='../../data/cleaned_data_prepared_copy.pkl')
