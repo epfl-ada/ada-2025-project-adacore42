857601a05bc551a004b667b42fb523fe6d6a5cf0
Katia Todorov <katia.todorov@epfl.ch>
2025-12-18 10:56:47 +0100

    katia intro web

--- _webappp/page_home.py
+++ _webappp/page_home.py
@@ -8,17 +8,31 @@ from _webappp.assets.app_definitions import AppParams as AP
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 
-st.title("Humor as a Mirror")
-
-"The New Yorker Captions as Reflections of Society, Politics, and Stereotypes"
+#st.title("Humor as a Mirror")
+st.markdown(
+    "<h1 style='text-align: center;'>Humor as a Mirror</h1>",
+    unsafe_allow_html=True
+)
+#"The New Yorker Captions as Reflections of Society, Politics, and Stereotypes"
+#"A reflection of society through comedy
+st.markdown(
+    "<h3 style='text-align: center;'>A reflection of Society, Politics, and Stereotypes</h3>",
+    unsafe_allow_html=True
+)
+st.image("data/newyorker_caption_contest_virgin/images/733.jpg", width=700)
+#"They don't hate us because we're wolves; they hate us because we're lawyers."
+st.markdown(
+    "<h5 style='text-align: center;'>They don't hate us because we're wolves; they hate us because we're lawyers.</h5>",
+    unsafe_allow_html=True)
 st.divider()
 
 st.write(
     """
-    This web app analyses thousands of captions from the *New Yorker* Caption Contest 
-    to understand what people find funny, and how humor reflects patterns in society.  
-    Using large-scale statistics, NLP tools, and semantic embeddings, we explore how
-    jokes encode stereotypes, power dynamics, and cultural norms.
+    Humor is a universal yet complex form of expression. The more we think about it and the more complexes it appear! There are numerous genres of humor, and it can be used in almost every life context, often depending on who is involved and the social setting. Humor can serve serious purposes, such as addressing sensitive or difficult topics and easing social tensions.
+
+    With this in mind, this project aim to investigate how jokes reflect societal traits and values. We will begin by examining what is generally perceived as “funny” and “unfunny,” and then explore how humor operates on two main topic : politics and gender. 
+
+    In order to answer this questions, we will analyze thousands of captions from the *New Yorker* Caption Contest. In this contest, a cartoon is published weekly or biweekly without a caption, and readers are invited to submit their own humorous responses. Other participants then vote on whether each caption is funny, somewhat funny, or unfunny. This process provides a rich dataset that captures public humor preferences and social attitudes. Perfect opportunity to explore how humor reflects societal norms!
     """
 )
 
@@ -30,47 +44,10 @@ st.divider()
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 
-dataNpipeline_c = st.container ()
+
+#dataNpipeline_c = st.container ()
 methodsNglance_c = st.container ()
 axisContent_c = st.container()
-
-
-
-# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-# Containers definition 
-# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-
-
-with dataNpipeline_c:
-    st.subheader("Dataset and Pipeline Overview")
-
-    col_1, col_2 = st.columns (2)
-
-    with col_1:
-        with st.expander("**Data sources**", expanded=AP.expanders):
-            st.write(
-                """
-                - 380+ caption contests  
-                - CSV caption tables, metadata JSON  
-                - Occupation list (≈33k curated roles)  
-                - Gendered lexicon from *Jailbreak the Patriarchy*  
-                """
-            )
-
-    with col_2:
-        with st.expander("**Processing pipeline**", expanded=AP.expanders):
-            st.write(
-                """
-                1. Centralised path + loading utilities  
-                2. Data cleaning and consistency checks  
-                3. Construction of a robust *funny score*  
-                4. Tokenisation and lemmatisation (optional)  
-                5. Models for similarity, clustering and gender analysis  
-                """
-            )
-
-    st.divider()
-
 
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
@@ -85,6 +62,15 @@ with methodsNglance_c:
 
     "**Occupation & Gender Lexicons**"
     st.info("Built to detect job references and gender indicators in captions and metadata.")
+
+
+    st.write(
+    """
+    If you are interested by learning more about the dataset, the processing pipeline and methods used, please check the sections below.
+    """
+    )
+    
+    #st.page_link("page_methods.py", label="Go to Methods Page", icon="➡️") # do not work yet
 
 
 
@@ -122,11 +108,14 @@ with axisContent_c:
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 
-
-
-st.info(
+st.divider()
+st.subheader("Conclusion")
+st.write(
+    """
+    ......
     "This app shows not just which captions win — "
     "but what our jokes reveal about culture, bias, and how society laughs."
+    """
 )
 
 
